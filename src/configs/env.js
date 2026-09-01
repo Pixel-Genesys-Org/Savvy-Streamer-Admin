6f11ffd0355b5988984cd1c486651af30ce71b70
Ethan Miller <ethanmillerdev676@gmail.com>
2026-09-01 17:16:49 +0500

    Update env.js

--- src/configs/env.js
+++ src/configs/env.js
@@ -1,6 +1,5 @@
 const CRYPTO_KEY = "rCRrt08XYmYZrUoKffOQWXa327AJyBxv"
-const BASE_URL = "https://streamsmart.projectstagingzone.com/stream-smart/v1/api"
-const IMAGE_BASE_URL = "https://streamsmart.projectstagingzone.com/"
+const BASE_URL = "https://api.savvystreamer.net/stream-smart/v1/api"
 const REACT_APP_MODE = "dev"
 
-export { CRYPTO_KEY, BASE_URL, IMAGE_BASE_URL, REACT_APP_MODE }
+export { CRYPTO_KEY, BASE_URL, REACT_APP_MODE }
